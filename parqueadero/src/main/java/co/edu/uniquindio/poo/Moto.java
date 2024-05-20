fb04c37529132b5b4429c51f059556164275ac12
SSS303330 <sebasvg3001@gmail.com>
2024-05-20 15:53:27 -0500

    Clase Moto

---
+++ parqueadero/src/main/java/co/edu/uniquindio/poo/Moto.java
@@ -0,0 +1,30 @@
+package parqueadero2;
+
+public class Moto extends Vehiculo {
+    private int velocidadMaxima; // La velocidad máxima que alcanza la moto.
+
+    /**
+     * @param placa La placa de la moto.
+     * @param modelo El modelo de la moto.
+     * @param propietario Dueño de la moto.
+     * @param velocidadMaxima La velocidad máxima de la moto.
+     */
+    public Moto(String placa, String modelo, String propietario, int velocidadMaxima) {
+        super(placa, modelo, propietario);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    /**
+     * Método para obtener el tipo de vehículo (moto)
+     * @return 
+     */
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    @Override
+    public int getTipo() {
+        return Parqueadero.TIPO_MOTO_CLASICA; // retorna el tipo de moto
+    }
+    
+}
