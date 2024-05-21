abc837f6a46c8c41ab7134317343caad83fe3c02
jharpi <162352351+jharpi@users.noreply.github.com>
2024-05-20 21:38:18 -0500

    AppTest

--- parqueadero/src/test/java/co/edu/uniquindio/poo/AppTest.java
+++ parqueadero/src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -1,29 +1,38 @@
-/**
- * Clase para probar el funcionamiento del código
- * @author Área de programación UQ
- * @since 2023-08
- * 
- * Licencia GNU/GPL V3.0 (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE) 
- */
 package co.edu.uniquindio.poo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.util.logging.Logger;
-import org.junit.jupiter.api.Test;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
-    private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
     /**
-     * Rigorous Test :-)
+     * @return the suite of tests being tested
      */
-    @Test
-    public void shouldAnswerWithTrue() {
-        LOG.info("Iniciado test shouldAnswerWithTrue");
-        assertTrue(true);
-        LOG.info("Finalizando test shouldAnswerWithTrue");
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
     }
 }
